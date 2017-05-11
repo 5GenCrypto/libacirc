d2fdcb1a0905f5831ba141d66c49d23833df6dc0
Alex J. Malozemoff <amaloz@galois.com>
2017-05-10 17:00:28 -0700

    remove rop from acirc_eval_mpz_mod_memo

--- src/gmp.c
+++ src/gmp.c
@@ -8,76 +8,68 @@
 #include <stdlib.h>
 #include <string.h>
 
-void acirc_eval_mpz_mod_memo(mpz_t rop, acirc *c, acircref root,
-                             const mpz_t *xs, const mpz_t *ys,
-                             const mpz_t modulus, bool *known,
-                             mpz_t *cache)
+void
+acirc_eval_mpz_mod_memo(acirc *c, acircref root, mpz_t *xs, mpz_t *ys,
+                        const mpz_t modulus, bool *known, mpz_t *cache)
 {
-    if (known[root]) {
-        mpz_set(rop, cache[root]);
+    if (known[root])
         return;
-    }
 
     const acirc_gate_t *gate = &c->gates.gates[root];
     const acirc_operation op = gate->op;
+    mpz_t *rop;
     switch (op) {
     case OP_INPUT:
-        mpz_set(rop, xs[gate->args[0]]);
+        rop = &xs[gate->args[0]];
         break;
     case OP_CONST:
-        mpz_set(rop, ys[gate->args[0]]);
+        rop = &ys[gate->args[0]];
         break;
     case OP_ADD: case OP_SUB: case OP_MUL: {
-        mpz_t zs[gate->nargs];
+        mpz_init(cache[root]);
+        rop = &cache[root];
         for (size_t i = 0; i < gate->nargs; ++i) {
-            mpz_init(zs[i]);
-            acirc_eval_mpz_mod_memo(zs[i], c, gate->args[i], xs, ys, modulus, known, cache);
+            acirc_eval_mpz_mod_memo(c, gate->args[i], xs, ys, modulus, known, cache);
         }
         if (op == OP_ADD) {
-            mpz_set_ui(rop, 0);
+            mpz_set_ui(*rop, 0);
             for (size_t i = 0; i < gate->nargs; ++i) {
-                mpz_add(rop, rop, zs[i]);
-                mpz_mod(rop, rop, modulus);
+                mpz_add(*rop, *rop, cache[gate->args[i]]);
+                mpz_mod(*rop, *rop, modulus);
             }
         } else if (op == OP_SUB) {
-            mpz_set(rop, zs[0]);
+            mpz_set(*rop, cache[gate->args[0]]);
             for (size_t i = 1; i < gate->nargs; ++i) {
-                mpz_sub(rop, rop, zs[i]);
-                mpz_mod(rop, rop, modulus);
+                mpz_sub(*rop, *rop, cache[gate->args[i]]);
+                mpz_mod(*rop, *rop, modulus);
             }
         } else if (op == OP_MUL) {
-            mpz_set_ui(rop, 1);
+            mpz_set_ui(*rop, 1);
             for (size_t i = 0; i < gate->nargs; ++i) {
-                mpz_mul(rop, rop, zs[i]);
-                mpz_mod(rop, rop, modulus);
+                mpz_mul(*rop, *rop, cache[gate->args[i]]);
+                mpz_mod(*rop, *rop, modulus);
             }
         } else abort();
-        for (size_t i = 0; i < gate->nargs; ++i) {
-            mpz_clear(zs[i]);
-        }
-
-        mpz_init(cache[root]);
-        mpz_set(cache[root], rop);
         known[root] = true;
         break;
     }
     case OP_SET:
-        acirc_eval_mpz_mod_memo(rop, c, gate->args[0], xs, ys, modulus, known, cache);
+        acirc_eval_mpz_mod_memo(c, gate->args[0], xs, ys, modulus, known, cache);
         break;
     case OP_EXTERNAL:
         abort();
     }
 }
 
-
-void acirc_eval_mpz_mod(mpz_t rop, acirc *c, acircref root,
-                        const mpz_t *xs, const mpz_t *ys,
-                        const mpz_t modulus)
+void
+acirc_eval_mpz_mod(mpz_t rop, acirc *c, acircref root, mpz_t *xs, mpz_t *ys,
+                   const mpz_t modulus)
 {
     bool known[acirc_nrefs(c)];
     mpz_t cache[acirc_nrefs(c)];
     memset(known, '\0', sizeof known);
-    acirc_eval_mpz_mod_memo(rop, c, root, xs, ys, modulus, known, cache);
+    acirc_eval_mpz_mod_memo(c, root, xs, ys, modulus, known, cache);
+    mpz_set(rop, cache[root]);
     for (size_t i = 0; i < acirc_nrefs(c); ++i) {
         if (known[i])
             mpz_clear(cache[i]);
@@ -116,8 +108,7 @@ bool acirc_ensure_mpz(acirc *c)
         for (size_t i = 0; i < c->ninputs; ++i)
             mpz_set_ui(xs[i], tests->inps[test_num][i]);
         for (size_t i = 0; i < c->outputs.n; i++) {
-            acirc_eval_mpz_mod(rs[i], c, c->outputs.buf[i], (const mpz_t *) xs,
-                               (const mpz_t *) ys, modulus);
+            acirc_eval_mpz_mod(rs[i], c, c->outputs.buf[i], xs, ys, modulus);
             test_ok = test_ok && (mpz_cmp_ui(rs[i], tests->outs[test_num][i]) == 0);
         }
 
